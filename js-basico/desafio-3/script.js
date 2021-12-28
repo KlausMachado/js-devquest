659d5ebe4c934049ee3740a889fed4781bea1e5f
Carla Machado <carlamrosa438@gmail.com>
2021-12-28 20:37:39 -0300

    desafio3 commit

---
+++ js-basico/desafio-3/script.js
@@ -0,0 +1,11 @@
+let respostaDoUsuario = confirm("Seja bem vindo(a) aventureiro(a)! Gostaria de pagar R$ 50,00 para passara noite na nossa hospedagem?")
+
+let ok = 'Ótimo! Nós temos as melhores camas de toda a região!'
+
+let cancelar = "Que pena! Você parecia ser uma pessoa mais legal"
+
+if (respostaDoUsuario) {
+    alert(ok)
+} else {
+    alert(cancelar)
+}
