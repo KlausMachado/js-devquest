2ac07da97524650e5f3ac41a7992c51e4279b2f1
Carla Machado <carlamrosa438@gmail.com>
2022-01-14 17:41:41 -0300

    updates3

--- js-intermediario/desafio1/script.js
+++ js-intermediario/desafio1/script.js
@@ -1,30 +1,28 @@
 "use strict";
+let contador = 0;
 const imagens = document.querySelectorAll(".imagem");
 
 function avancar() {
-  for (let contador = 0; contador < imagens.length; contador++) {
-    console.log("ate aqui 1");
-    imagens[contador].classList.add("mostrar");
+  console.log("ate aqui 1");
+  imagens[contador].classList.add("mostrar");
+  contador++;
 
-    if (contador >= 3) {
-      let setaOpaca = document.getElementById("seta-direita");
-      setaOpaca.classList.add("opacidade");
-    }
+  if (contador >= 3) {
+    let setaOpaca = document.getElementById("seta-direita");
+    setaOpaca.classList.add("opacidade");
   }
 }
 
 function voltar() {
-  for (let contador = 3; contador >= 0; contador--) {
-    console.log("ate aqui 2");
-    imagens[contador].classList.add("mostrar");
+  console.log("ate aqui 2");
+  imagens[contador].classList.add("mostrar");
+  contador--;
 
-    if (contador <= 0) {
-      let setaOpaca2 = document.getElementById("seta-esquerda");
-      setaOpaca2.classList.add("opacidade");
-    }
+  if (contador <= 0) {
+    let setaOpaca2 = document.getElementById("seta-esquerda");
+    setaOpaca2.classList.add("opacidade");
   }
 }
-console.log("ate aqui 3");
 
 document.getElementById("seta-direita").addEventListener("click", avancar);
 
