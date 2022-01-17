4e32bd92fcf049240c05572433b1186b09dd7a61
Carla Machado <carlamrosa438@gmail.com>
2022-01-17 01:59:15 -0300

    finalizado

--- js-intermediario/desafio1/script.js
+++ js-intermediario/desafio1/script.js
@@ -1,27 +1,61 @@
 "use strict";
 let contador = 0;
+let setaEsquerdaOpaca = document.getElementById("seta-esquerda-img");
+let setaDireitaOpaca = document.getElementById("seta-direita-img");
+let link = document.getElementById("saibaMais");
+let titulo = document.querySelector("h1");
 const imagens = document.querySelectorAll(".imagem");
 
-function avancar() {
-  console.log("ate aqui 1");
-  imagens[contador].classList.add("mostrar");
-  contador++;
+function esconderImagem() {
+  imagens.forEach((imagens) => {
+    imagens.classList.remove("mostrar");
+  });
+}
 
-  if (contador >= 3) {
-    let setaOpaca = document.getElementById("seta-direita");
-    setaOpaca.classList.add("opacidade");
+function corLink() {
+  if (contador == 0) {
+    titulo.innerHTML = "Titulo imgem 1";
+    link.style.color = "#c50000";
+  } else if (contador == 1) {
+    titulo.innerHTML = "Titulo imgem 2";
+    link.style.color = " #D9DD10";
+  } else if (contador == 2) {
+    titulo.innerHTML = "Titulo imgem 3";
+    link.style.color = " #A63A17";
+  } else {
+    titulo.innerHTML = "Titulo imgem 4";
+    link.style.color = " #10ACDD";
   }
 }
 
-function voltar() {
-  console.log("ate aqui 2");
+function mostrarImagem() {
   imagens[contador].classList.add("mostrar");
-  contador--;
+}
 
-  if (contador <= 0) {
-    let setaOpaca2 = document.getElementById("seta-esquerda");
-    setaOpaca2.classList.add("opacidade");
+function avancar() {
+  if (contador === imagens.length - 1) {
+    setaDireitaOpaca.classList.add("opacidade");
+    return;
   }
+
+  contador++;
+  setaEsquerdaOpaca.classList.remove("opacidade");
+  esconderImagem();
+  mostrarImagem();
+  corLink();
+}
+
+function voltar() {
+  if (contador === 0) {
+    setaEsquerdaOpaca.classList.add("opacidade");
+    return;
+  }
+
+  contador--;
+  setaDireitaOpaca.classList.remove("opacidade");
+  esconderImagem();
+  mostrarImagem();
+  corLink();
 }
 
 document.getElementById("seta-direita").addEventListener("click", avancar);
